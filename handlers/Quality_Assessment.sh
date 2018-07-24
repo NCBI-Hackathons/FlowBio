f148354dbf2311ecd19af45790a584bce98fbfc6
ChaochihL <liux1299@umn.edu>
2018-07-24 15:53:29 -0400

    add colon

--- handlers/Quality_Assessment.sh
+++ handlers/Quality_Assessment.sh
@@ -18,7 +18,7 @@ function summarizeQC() {
     local project="$4" # The name of the project
     local sample_name="$(basename ${zip_file} _fastqc.zip)" # The name of the sample
     local zip_dir="$(basename ${zip_file} .zip)" # The name of the directory after unzipping
-    (set -x; unzip -q "${zip_file}" -d "$(dirname ${zip_dir})") # Unzip the zip file
+    unzip -q "${zip_file}" -d "$(dirname ${zip_dir})" # Unzip the zip file
     # Get PASS/WARN/FAIL data from the summary.txt file
     local PerBaseSequenceQuality=$(grep "Per base sequence quality" ${zip_dir}/summary.txt | cut -f 1)
     local PerTileSequenceQuality=$(grep "Per tile sequence quality" ${zip_dir}/summary.txt | cut -f 1)
@@ -47,9 +47,9 @@ function summarizeQC() {
     fi
     # Write the sequence data to the summary file
     echo -e "${sample_name}\t${Encoding}\t${ReadLength}\t${ReadCount}\t${ReadDepth}\t${GC}\t${PercentDeduplicated}\t${PerBaseSequenceQuality}\t${PerTileSequenceQuality}\t${PerSequenceQualityScores}\t${PerBaseSequenceContent}\t${PerSequenceGCContent}\t${PerBaseNContent}\t${SequenceLengthDistribution}\t${SequenceDuplicationLevels}\t${OverrepresentedSequences}\t${AdapterContent}\t${KmerContent}" >> "${out_dir}/${project}_quality_summary_unfinished.txt"
-    (set -x; rm -rf "${zip_dir}") # Remove the unzipped directory
-    (set -x; mv "${out_dir}/${sample_name}_fastqc.html" "${out_dir}/HTML_Files/") # Move the HTML file for this sample
-    (set -x; mv "${out_dir}/${sample_name}_fastqc.zip" "${out_dir}/Zip_Files/") # Move the zip file for this sample
+    rm -rf "${zip_dir}" # Remove the unzipped directory
+    mv "${out_dir}/${sample_name}_fastqc.html" "${out_dir}/HTML_Files/" # Move the HTML file for this sample
+    mv "${out_dir}/${sample_name}_fastqc.zip" "${out_dir}/Zip_Files/" # Move the zip file for this sample
 }
 
 export -f summarizeQC
@@ -61,13 +61,13 @@ function Main_Quality_Assessment_FastQC() {
     local project="$3" # What do we call our results?
     local size="$4" # What is the size of the covered region?
     mkdir -p "${out_dir}/HTML_Files" "${out_dir}/Zip_Files" # Make our out_dirput directories
-    cat "${sampleList}" | parallel "fastqc --out_dirdir ${out_dir} {}" # Run FastQC in parallel
+    cat "${sampleList}" | parallel "fastqc --out_dir ${out_dir} {}" # Run FastQC in parallel
     # Make a list of all the zip files
     local zipList=$(find "${out_dir}" -name "*.zip" | sort)
     # Add the header to the quality summary file
     echo -e "Sample name\tEncoding\tRead length\tNumber of reads\tRead depth\t%GC\tDeduplicated percentage\tPer base sequence quality\tPer tile sequence quality\tPer sequence quality scores\tPer base sequence content\tPer sequence GC content\tPer base N content\tSequence length distribution\tSequence duplication levels\tOverrepresented sequences\tAdapter content\tKmer content" > "${out_dir}/${project}_quality_summary_unfinished.txt"
     # Calculate stats and add a row to the summary file for each sample
-    parallel -v summarizeQC {} "${size}" "${out_dir}" "${project}" ::: "${zipList}"
+    parallel -v summarizeQC {} "${size}" "${out_dir}" "${project}" :::: "${zipList}"
     # Add the header to a new file to contain the sorted list
     echo -e "Sample name\tEncoding\tRead length\tNumber of reads\tRead depth\t%GC\tDeduplicated percentage\tPer base sequence quality\tPer tile sequence quality\tPer sequence quality scores\tPer base sequence content\tPer sequence GC content\tPer base N content\tSequence length distribution\tSequence duplication levels\tOverrepresented sequences\tAdapter content\tKmer content" > "${out_dir}/${project}_quality_summary.txt"
     # Sort the summary file based on sample name
